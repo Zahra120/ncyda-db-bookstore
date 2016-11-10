5a042fad12186f46603a93036eb556256ec9e4b3
Izel Nakri <contact@izelnakri.com>
2016-11-10 14:06:10 +0100

    Books CRUD application created

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require('express'),
       morgan = require('morgan'),
       bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
       pug = require('pug'),
       Sequelize = require('sequelize');
 
 var app = express(),
     sequelize = new Sequelize('izelnakri', 'izelnakri', '', { dialect: 'postgres' });
 
-// Our model definition:
-var Book = sequelize.define('book', {
-  title: Sequelize.STRING,
-  imageURL: Sequelize.STRING,
-  author: Sequelize.STRING,
-  description: Sequelize.TEXT
-});
-// ======================
+var booksRouter = require('./routes/books');
+
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }})
+);
 
 app.set('view engine', 'pug');
 
@@ -27,27 +30,7 @@ app.get('/', (request, response) => {
   response.redirect('/books');
 });
 
-app.get('/books', (request, response) => {
-  Book.findAll().then((books) => {
-    response.render('books/index', { books: books });
-  });
-});
-
-app.post('/books', (request, response) => {
-  Book.create(request.body).then(() => {
-    response.redirect('/books');
-  });
-});
-
-app.get('/books/new', (request, response) => {
-  response.render('books/new');
-});
-
-app.get('/books/:id/edit', (request, response) => {
-  Book.findById(request.params.id).then((book) => {
-    response.render('books/edit', { book: book });    
-  });
-});
+app.use('/books', booksRouter);
 
 sequelize.sync().then(() => {
   console.log('Connected to database');
